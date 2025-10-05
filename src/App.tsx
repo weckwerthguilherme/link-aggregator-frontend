7a77faf599afbc13fdc9e2a96331f089caaaa17b
Guilherme <weckwerthguilherme@gmail.com>
2025-10-05 19:21:46 -0300

    Integracao com o backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
+import { useState, useEffect } from "react";
 import ListGroup from "./components/ListGroup";
-import ButtonExercitar from "./components/ButtonExercitar";
+import axios from "axios";
+
+interface Link {
+  id: number;
+  title: string;
+  url: string;
+}
 
 function App() {
-  let items = ["Github", "LinkedIn", "Discord", "Instagram"];
+  const [links, setLinks] = useState<Link[]>([]);
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  useEffect(() => {
+    const apiUrl = `${import.meta.env.VITE_API_URL}/links`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setLinks(response.data);
+      })
+      .catch((error) => {
+        console.error("Houve um erro ao buscar os links:", error);
+      });
+  }, []);
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Links"
-        onSelectItem={handleSelectItem}
-      />
-      <ButtonExercitar color="warning" onClick={() => console.log("clicado")}>
-        Meu botao
-      </ButtonExercitar>
+      <ListGroup items={links} heading="Meus Links" />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,40 +4,47 @@
 
 import { useState } from "react";
 
+interface Link {
+  id: number;
+  title: string;
+  url: string;
+}
+
 interface Props {
-  items: string[];
+  items: Link[];
   heading: string;
-  onSelectItem: (item: string) => void;
 }
 
 // Props são os inputs passados pra um componente similar a um argumento de função, deve ser IMUTAVEL
 // State são os dados manuseados pelo componente, como se fosse uma variavel local dentro de uma função, deve ser MUTAVEL
 
-function ListGroup({ items, heading, onSelectItem }: Props) {
+function ListGroup({ items, heading }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>{heading}</h1>
-      {items.length === 0 && <p>No Item Found</p>}
+      {items.length === 0 && (
+        <p>Nenhum Link Encontrado (O backend está rodando?)</p>
+      )}
       <ul className="list-group">
-        {items.map((item, index) => (
-          <li
+        {items.map((link, index) => (
+          <a
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
             className={
               selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
+                ? "list-group-item list-group-item-action active"
+                : "list-group-item list-group-item-action"
             }
-            key={item}
+            key={link.id}
             onMouseOver={() => {
               setSelectedIndex(index);
             }}
-            onClick={() => {
-              onSelectItem(item);
-            }}
           >
-            {item}
-          </li>
+            {link.title}
+          </a>
         ))}
       </ul>
     </>
